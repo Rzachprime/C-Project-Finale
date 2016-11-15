433189353a823d074d9ef1fa98dc7747fb3b5ebe
Rzach <Rzachprime@gmail.com>
2016-11-15 17:56:04 -0500

    Updated the main page text

---
+++ ProjectFinale/ProjectFinale/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectFinale.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is the page where you view your Army";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Me!";
+
+            return View();
+        }
+    }
+}
